1291f26d5ccfc762f57b0204ab5bddee52efb8ff
muchpretty99 <muchpretty99@ibizsoftware.net>
2023-07-28 13:58:20 +0900

    axios를 이용해서 넷플릭스 클론 코딩

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "27910295775a7a672631f156e2b31bc3",
+    language: "ko-KR",
+  },
+});
+
+export default instance;
