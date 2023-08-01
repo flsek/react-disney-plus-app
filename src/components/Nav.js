1fde29c16a6f19959feeaf8c21c872579207714e
muchpretty99 <muchpretty99@ibizsoftware.net>
2023-08-01 11:01:28 +0900

    useRef 적용해서 모달 밖에 눌렀을 때도 모달 사라지게 하기

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const Nav = () => {
   const [show, setShow] = useState(false);
-
+  const { pathname } = useLocation();
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (window.scrollY > 50) {
-        setShow(true);
-      } else {
-        setShow(false);
-      }
+      handleScroll();
     });
 
     return () => {
-      window.removeEventListener("scroll", () => {});
+      window.removeEventListener("scroll", () => {
+        handleScroll();
+      });
     };
   }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <NavWrapper show={show}>
       <Logo>
@@ -28,11 +43,50 @@ const Nav = () => {
           }}
         />
       </Logo>
+
+      {pathname === "/" ? (
+        <Login>Login</Login>
+      ) : (
+        <Input
+          className="nav_input"
+          type="text"
+          placeholder="검색해주세요."
+          value={searchValue}
+          onChange={handleChange}
+        />
+      )}
     </NavWrapper>
   );
 };
 
 export default Nav;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: gray;
+    border-color: transparent;
+  }
+`;
+
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%, 0);
+  background-color: rgba(0, 0, 0, 0.582);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: none;
+`;
+
 const NavWrapper = styled.nav`
   position: fixed;
   top: 0;
